2aac3360cfeb0c8c4ebd416e1998a6b238b8e2dd
Patricio <pat.venegas.l@gmail.com>
2018-04-11 17:11:41 -0300

    se agregan archivos restantes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+
+
+  def home
+  end
+
+  def search
+  	@user = User.where.not(id: current_user.id).sample
+
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  get 'search', to: 'pages#search'
+
+  resources :interactions, only: :create
+
+   root 'pages#search'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
